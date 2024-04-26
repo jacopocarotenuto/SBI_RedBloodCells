2e1968293a8df8acc7d2388e4bcee71bca831f9b
Lorenzo Vigorelli <lorenzo.vigorellikd35@gmail.com>
2024-04-26 19:41:33 +0200

    Implemented the summary statistics cocatanation Cxx and S_red

--- Utils_functions.py
+++ Utils_functions.py
@@ -2,7 +2,7 @@
 from numba import jit
 from numpy import zeros, arange, uint8, int32, float32, sqrt, uint32, ones, int64, mean, ceil, where, log2, max, min
 from numpy.random import randn
-from numpy.fft import fft, ifft
+from numpy.fft import fft, ifft, fftfreq
 
 @jit(nopython = True)
 def Simulator_noGPU(dt, DeltaT, TotalT, n_sim, theta, i_state = None):
@@ -40,7 +40,8 @@ def Simulator_noGPU(dt, DeltaT, TotalT, n_sim, theta, i_state = None):
     # Initialize x_trace array
     
     x_trace = zeros((n_sim, sampled_point_amount), dtype = float32)
-    
+    f_trace = zeros((n_sim, sampled_point_amount), dtype = float32)
+
     sampling_counter = int64(1)
     
     # POSSIBLE OPTIM: You could overwrite the last used row of the trace to save memory and not create a proxy array
@@ -57,39 +58,79 @@ def Simulator_noGPU(dt, DeltaT, TotalT, n_sim, theta, i_state = None):
         sampling_counter = sampling_counter + 1
         if sampling_counter == sampling_delta_time_steps:
             x_trace[:, int(t/sampling_delta_time_steps)] = x[:,0]
+            f_trace[:, int(t/sampling_delta_time_steps)] = f[:,0]
+            
             sampling_counter = int64(1)
-    
-    return x_trace, (x, y, f) # Check if this is right
+
+    return x_trace, f_trace, (x, y, f) # Check if this is right
 
 def corr(x,y,nmax,dt=False):
     '''fft, pad 0s, non partial'''
 
-    assert len(x)==len(y)
+    assert len(x)==len(y), 'x and y must have the same length'
 
     n=len(x)
     # pad 0s to 2n-1
     ext_size=2*n-1
     # nearest power of 2
-    fsize=2**np.ceil(np.log2(ext_size)).astype('int')
+    fsize=2**ceil(log2(ext_size)).astype('int')
 
-    xp=x-np.mean(x)
-    yp=y-np.mean(y)
+    xp=x-mean(x)
+    yp=y-mean(y)
 
     # do fft and ifft
-    cfx=np.fft.fft(xp,fsize)
-    cfy=np.fft.fft(yp,fsize)
+    cfx=fft(xp,fsize)
+    cfy=fft(yp,fsize)
     if dt != False:
-        freq = np.fft.fftfreq(n, d=dt)
-        idx = np.where((freq<-1/(2*dt))+(freq>1/(2*dt)))[0]
+        freq = fftfreq(n, d=dt)
+        idx = where((freq<-1/(2*dt))+(freq>1/(2*dt)))[0]
         
         cfx[idx]=0
         cfy[idx]=0
         
     sf=cfx.conjugate()*cfy
-    corr=np.fft.ifft(sf).real
+    corr=ifft(sf).real
     corr=corr/n
 
     return corr[:nmax]
 
 def normalize_numpy(array):
-    return (array - np.min(array)) / (np.max(array) - np.min(array))
+    return (array - min(array)) / (max(array) - min(array))
+
+
+def CheckParameters(dt, DeltaT, TotalT, theta):
+    time_steps_amount = int64(TotalT/dt) # Number of steps
+    sampled_point_amount = int64(TotalT/DeltaT) # Number of sampled points
+    sampling_delta_time_steps = int64(DeltaT/dt) # Number of steps between samples
+    n_sim = theta[0].shape[0]
+    # Aggiugnere controllo sul TotalT effettivo a fine simulazione
+    # Aggiungere controllo sul sampling_delta_time_steps per sanity check
+    # Controllare che sampled_point_amount*sampling_delta_time_steps = time_steps_amount
+    
+    print(f"Your Integration Time (dt) is {dt:.2E} seconds")
+    print(f"Your Sampling Time (Delta) is {DeltaT:.2E} seconds, corresponding to a {1/DeltaT:.2f}Hz sampling frequency")
+    print(f"Your Total Simulation Time is {TotalT:.2E} seconds")
+    print(f"Your Number of Simulated Trajectories is {n_sim:.2E}")
+    print(f"The amount of total time steps is {time_steps_amount:.2E}")
+    print(f"The amount of sampled points is {sampled_point_amount:.2E}")
+    print(f"The gap between two sampled points is {sampling_delta_time_steps:.1E} time steps")
+    passed_sanity_checks = True
+    print("---- SANITY CHECKS ----")
+    if TotalT != DeltaT*sampled_point_amount:
+        print(f"WARNING: TotalT is {TotalT}s, but DeltaT*sampled_point_amount is {DeltaT*sampled_point_amount}s")
+        passed_sanity_checks = False
+    if sampled_point_amount*sampling_delta_time_steps != time_steps_amount:
+        print(f"WARNING: sampled_point_amount*sampling_delta_time_steps is {sampled_point_amount*sampling_delta_time_steps}, but time_steps_amount is {time_steps_amount}")
+        passed_sanity_checks = False
+    if time_steps_amount*dt != TotalT:
+        print(f"WARNING: time_steps_amount*dt is {time_steps_amount*dt}, but TotalT is {TotalT}")
+        passed_sanity_checks = False
+    if dt*sampling_delta_time_steps != DeltaT:
+        print(f"WARNING: dt*sampling_delta_time_steps is {dt*sampling_delta_time_steps}, but DeltaT is {DeltaT}")
+        passed_sanity_checks = False
+    if len(set([x.shape for x in theta])) != 1:
+        raise Exception("Parameters dimension are not all equal. Detected number of different parameters: ", n_sim)
+    if passed_sanity_checks:
+        print("All checks passed")
+        
+    return None
